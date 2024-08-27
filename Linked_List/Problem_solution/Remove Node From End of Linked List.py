9e995c5423f8b5a661acfde5c94b01ff00b515f7
SiddharthShipalkar <siddharthshipalkar9404@gmail.com>
2024-08-27 20:38:37 +0530

    add Q in removed node from Nth place

--- Linked_List/Problem_solution/Remove Node From End of Linked List.py
+++ Linked_List/Problem_solution/Remove Node From End of Linked List.py
@@ -1,27 +1,31 @@
-# Linked List Cycle Detection
+# Remove Node From End of Linked List
+# Solved 
+# You are given the beginning of a linked list head, and an integer n.
 
-# Given the beginning of a linked list head, return true if 
-# there is a cycle in the linked list. Otherwise, return false.
-# There is a cycle in a linked list if at least one node in the list that 
-# can be visited again by following the next pointer.
-# Internally, index determines the index of the beginning of the cycle,
-# if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, 
-# then the tail node points to null and no cycle exists.
-# Note: index is not given to you as a parameter.
+# Remove the nth node from the end of the list and return the beginning of the list.
 
 # Example 1:
-# Input: head = [1,2,3,4], index = 1
-# Output: true
-# Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
 
+# Input: head = [1,2,3,4], n = 2
+
+# Output: [1,2,4]
 # Example 2:
-# Input: head = [1,2], index = -1
-# Output: false
 
+# Input: head = [5], n = 1
+
+# Output: []
+# Example 3:
+
+# Input: head = [1,2], n = 2
+
+# Output: [2]
 # Constraints:
-# 1 <= Length of the list <= 1000.
-# -1000 <= Node.val <= 1000
-# index is -1 or a valid index in the linked list.
+
+# The number of nodes in the list is sz.
+# 1 <= sz <= 30
+# 0 <= Node.val <= 100
+# 1 <= n <= sz
+
 
 
 
